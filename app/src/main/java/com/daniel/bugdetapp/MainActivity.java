4d02d99e2fd174210ac3e4f5bfc3e8ae26160654
DanielHaggstrom <daniel.hpf@hotmail.com>
2019-03-06 00:09:26 +0100

    solved crash when launching app

--- app/src/main/java/com/daniel/bugdetapp/MainActivity.java
+++ app/src/main/java/com/daniel/bugdetapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.daniel.bugdetapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
